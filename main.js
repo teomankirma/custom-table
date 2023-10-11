4e77b198a0b5c97fea1867efce4e7bf9c898a583
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2023-10-11 11:48:03 -0700

    feat: table functionality

--- main.js
+++ main.js
@@ -1,7 +1,55 @@
 import "./style.css";
 
 document.querySelector("#app").innerHTML = `
-  <div id="test">
-    <h1>Custom Table</h1>
+<div class="container">
+    <div>
+      <form>
+        <input class="name" type="text" placeholder="Enter your name" />
+        <select class="options">
+          <option value="">Select an option</option>
+          <option value="1">Option 1</option>
+          <option value="2">Option 2</option>
+        </select>
+        <button type="submit" class="submit-btn">Submit</button>
+      </form>
+      <table class="table">
+        <thead class="table-head">
+          <tr>
+            <th>Name</th>
+            <th>Option</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
   </div>
 `;
+
+$(document).ready(function () {
+  $(".submit-btn").click(function (e) {
+    e.preventDefault();
+    let name = $(".name").val();
+    let option = $(".options").val();
+    if (name == "" || option == "") {
+      alert("Please fill in all fields");
+    } else {
+      const currentContent = $(".table").html();
+      $(".table").html(
+        currentContent +
+          `<tr>
+            <td>${name}</td>
+            <td>${option}</td>
+            <td>
+              <button class="delete">Delete</button>
+            </td>
+          </tr>`
+      );
+      $(".name").val("");
+      $(".options").val("");
+    }
+  });
+});
+
+$(document).on("click", ".delete", function () {
+  $(this).closest("tr").remove();
+});
