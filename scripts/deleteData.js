1ebba46ad250626f766a2d738098935db4d6430f
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2023-10-11 12:14:08 -0700

    feat: delete logic error handling

--- scripts/deleteData.js
+++ scripts/deleteData.js
@@ -1,17 +1,29 @@
-import { ref, onValue, remove } from "firebase/database";
+import { ref, get, remove } from "firebase/database";
 import { database } from "./config";
 
-export const deleteData = (id) => {
+export const deleteData = async (id) => {
   const userDataRef = ref(database, "users/");
-  onValue(userDataRef, (snapshot) => {
+  const snapshot = await get(userDataRef);
+
+  if (snapshot.exists()) {
     const data = snapshot.val();
+    const matchingKey = Object.keys(data).find(
+      (key) => data[key].userId === id
+    );
 
     // Loop through the data and find the object with the matching id
-    for (let key in data) {
-      if (data[key].userId === id) {
-        // Delete the object
-        remove(ref(database, "users/" + key));
+    if (matchingKey) {
+      const userRef = ref(database, "users/" + matchingKey);
+      try {
+        await remove(userRef);
+        console.log("Data deleted successfully");
+      } catch (error) {
+        console.error("Error deleting data:", error);
       }
+    } else {
+      console.error("Data with ID not found:", id);
     }
-  });
+  } else {
+    console.error("Data not found at the specified location.");
+  }
 };
